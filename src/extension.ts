2e750a91d8d057a6bd607f6dd17afba1faa64a47
plainionist <stanley751@gmx.net>
2024-07-27 07:08:36 +0200

    code reformatted

--- src/extension.ts
+++ src/extension.ts
@@ -1,38 +1,32 @@
-'use strict';
+'use strict'
 
-import * as vscode from 'vscode';
-import {type, pause} from './humanTyper';
+import * as vscode from 'vscode'
+import { type, pause } from './humanTyper'
 
-let typewriterBuffer:string;
+let typewriterBuffer: string
 
 export function activate(context: vscode.ExtensionContext) {
+  let playTypewriterCmd = vscode.commands.registerCommand('typewriter.playback', () => {
+    let minSpeed = vscode.workspace.getConfiguration('typewriter').get<number>('TypingMinSpeed') | 30
+    let maxSpeed = vscode.workspace.getConfiguration('typewriter').get<number>('TypingMaxSpeed') | 120
+    type(typewriterBuffer, minSpeed, maxSpeed)
+  })
 
-    let playTypewriterCmd = vscode.commands.registerCommand('typewriter.playback', () => {
+  let setTypewriterCmd = vscode.commands.registerCommand('typewriter.setTypewriter', () => {
+    var editor = vscode.window.activeTextEditor
+    if (!editor) {
+      return // No open text editor
+    }
+    var selection = editor.selection
+    typewriterBuffer = editor.document.getText(selection)
+  })
 
+  let pausePlaybackCmd = vscode.commands.registerCommand('typewriter.pause', () => {
+    pause()
+  })
 
-        let minSpeed = vscode.workspace.getConfiguration('typewriter').get<number>('TypingMinSpeed') | 30;
-        let maxSpeed = vscode.workspace.getConfiguration('typewriter').get<number>('TypingMaxSpeed') | 120;
-        type(typewriterBuffer, minSpeed, maxSpeed);
-    });
-
-    let setTypewriterCmd = vscode.commands.registerCommand('typewriter.setTypewriter', () => {
-        var editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            return; // No open text editor
-        }
-        var selection = editor.selection;
-        typewriterBuffer = editor.document.getText(selection);
-    })
-
-    let pausePlaybackCmd = vscode.commands.registerCommand('typewriter.pause', () => {
-        pause();
-    });
-
-
-    context.subscriptions.push(playTypewriterCmd);
-    context.subscriptions.push(setTypewriterCmd);
+  context.subscriptions.push(playTypewriterCmd)
+  context.subscriptions.push(setTypewriterCmd)
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
--- src/humanTyper.ts
+++ src/humanTyper.ts
@@ -1,72 +1,68 @@
-import {window, workspace, Position, Selection, TextEditor} from 'vscode';
+import { window, workspace, Position, Selection, TextEditor } from 'vscode'
 
-
-const charsToPauseOn = [",", '.', '!'];
-let paused = false;
+const charsToPauseOn = [',', '.', '!']
+let paused = false
 
 export function pause() {
-    if (paused && resumeCall) {
-        resumeCall();
-        resumeCall = undefined;
-    }
-    paused = !paused;
+  if (paused && resumeCall) {
+    resumeCall()
+    resumeCall = undefined
+  }
+  paused = !paused
 }
 
-let resumeCall;
+let resumeCall
 
+export function type(textToInsert: string, minSpeed: number, maxSpeed: number) {
+  var editor = window.activeTextEditor
+  if (!editor) {
+    return // No open text editor
+  }
 
-export function type(textToInsert:string, minSpeed:number, maxSpeed:number) {
-    var editor = window.activeTextEditor;
-    if (!editor) {
-        return; // No open text editor
-    };
-
-    typeTextRecursive(textToInsert, editor, editor.selection.end, minSpeed, maxSpeed);
+  typeTextRecursive(textToInsert, editor, editor.selection.end, minSpeed, maxSpeed)
 }
 
-let getRandomArbitrary = (min:number, max:number) => {
-    return Math.random() * (max - min) + min;
+let getRandomArbitrary = (min: number, max: number) => {
+  return Math.random() * (max - min) + min
 }
 
-function typeTextRecursive(buffer:string, editor:TextEditor, pos:Position, minSpeed:number, maxSpeed:number) {
+function typeTextRecursive(buffer: string, editor: TextEditor, pos: Position, minSpeed: number, maxSpeed: number) {
+  let token = buffer.substring(0, 1)
+  if (buffer.length == 0) {
+    return
+  }
+  buffer = buffer.slice(1, buffer.length)
 
-    let token = buffer.substring(0, 1);
-    if (buffer.length == 0) {
-        return;
-    }
-    buffer = buffer.slice(1, buffer.length)
+  if (token == `\n` || token == `\r\n`) {
+    pos = new Position(pos.line + 1, 0) //start of a new line
+  }
 
+  window.activeTextEditor
+    .edit((editbuilder) => {
+      editbuilder.insert(pos, token)
 
-    if (token == `\n` || token == `\r\n`) {
-        pos = new Position(pos.line + 1, 0); //start of a new line
-    }
+      // move the cursor
+      var newSelection = new Selection(pos, pos)
+      editor.selection = newSelection
+    })
+    .then(() => {
+      let timeout = getRandomArbitrary(minSpeed, maxSpeed)
 
+      // after a pause char (like a coma), take a breath
+      if (charsToPauseOn.indexOf(token) != -1) {
+        timeout += maxSpeed * 1.5
+      }
 
-    window.activeTextEditor.edit((editbuilder) => {
-        editbuilder.insert(pos, token);
+      // increment the position
+      pos = new Position(pos.line, pos.character + token.length)
 
-        // move the cursor
-        var newSelection = new Selection(pos, pos);
-        editor.selection = newSelection;
-    }).then(() => {
-        let timeout = getRandomArbitrary(minSpeed, maxSpeed);
-
-        // after a pause char (like a coma), take a breath
-        if (charsToPauseOn.indexOf(token) != -1) {
-            timeout += maxSpeed * 1.5;
+      setTimeout(() => {
+        if (!paused) typeTextRecursive(buffer, editor, pos, minSpeed, maxSpeed)
+        else {
+          resumeCall = function () {
+            typeTextRecursive(buffer, editor, pos, minSpeed, maxSpeed)
+          }
         }
-
-        // increment the position
-        pos = new Position(pos.line, pos.character + token.length);
-
-        setTimeout(() => {
-            if (!paused)
-                typeTextRecursive(buffer, editor, pos, minSpeed, maxSpeed);
-            else {
-                resumeCall = function () {
-                    typeTextRecursive(buffer, editor, pos, minSpeed, maxSpeed);
-                }
-            }
-        }, timeout)
-    });
+      }, timeout)
+    })
 }
